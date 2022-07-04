12b96a11a618d229cde3a1498925465c6381eda4
DayOS <43686351+Day-OS@users.noreply.github.com>
2022-07-04 15:10:17 -0300

    .

--- src/vtncrw.cpp
+++ src/vtncrw.cpp
@@ -1,167 +1,43 @@
 #include "vtncrw.h"
+#include <cstring>
 #include "iostream"
-#include "vector"
-#include "string.h"
+#include "bitset"
 
-VTNCRW::VTNCRW(){}
-
-VTNCFile VTNCRW::create(u8c layerQuantity, Resolution EachLayerResolution[], u8c ColorsQuantity, u8c EndsAnimationAt)
+VTNCRW::VTNCFile::VTNCFile()
 {
-    VTNCFile file;
-    Layer layers[layerQuantity];
-    for (size_t i = 0; i < layerQuantity; i++)
+}
+
+
+namespace VTNCRW
+{
+    rescData::rescData(unsigned int bitsize, unsigned int initialsize)
     {
-        file.layersResolution[i] = EachLayerResolution[i];
-        Frame framearray[255]; 
-        for (size_t frameindex = 0; frameindex < EndsAnimationAt; frameindex++)
-        {        
-            layers[i].framesArray[frameindex].msDuration = 100;
-            layers[i].framesArray[frameindex].Pixels[file.layersResolution[i].x * file.layersResolution[i].y] = 0;
-        }
+        this->data = (unsigned char*) calloc (initialsize, sizeof(unsigned char));
+        this->bitSize = bitsize;
+    }
+
+    rescData::~rescData()
+    {
         
+    }
+    
+    
+    rescData rescData::operator+(rescData b)
+    {
+        rescData result = *this;
+        std::cout << "BINARY DATA: " << std::hex << *reinterpret_cast<std::bitset<8*4>*>(data) << std::endl;
+        std::cout<< strlen(reinterpret_cast<const char*>(data));
+        //result.data = (unsigned char *) 
+        //reinterpret_cast<std::bitset<8>*>( data[ strlen(reinterpret_cast<const char*>(data)) ]
+        //    )
+        // + 4;
+        //std::cout << "sus";
+
+
         
-        layers[i].layerKey = 0;
+        std::cout << "BINARY DATA AFTER: " << std::hex << *reinterpret_cast<std::bitset<8*4>*>(result.data) << std::endl;
+        //std::cout << std::endl << "length: " << strlen(reinterpret_cast<const char*>(b.data)) << std::endl;
+        return result;
     }
-    for (size_t i = 0; i < ColorsQuantity; i++)
-    {
-        RGBA CurrentColor;
-        CurrentColor.R = CurrentColor.G = CurrentColor.B = 0;
-        CurrentColor.A = 0xFF; 
-        file.Colors[i] = CurrentColor;
-    }
-    
-    file.isFile = true;
-    file.framesQuantity = EndsAnimationAt;
-    file.colorsQuantity = ColorsQuantity;
-    file.layersQuantity = layerQuantity;
-    file.Layers = layers;
-    return file;
 }
 
-void readChunk(std::vector<unsigned char> file, int &lastaddress, int bytesize, unsigned char *output, bool isString = false){
-     
-    for (size_t i = 0; i < bytesize; i++)
-    {
-        memset(output + i, file[lastaddress + i], 1);
-    }
-    if (isString)
-    {
-        lastaddress -= 1;
-        memset(output + bytesize-1, 0, 1);
-    }
-    lastaddress += bytesize;
-    return;
-}
-void readChunk16u(std::vector<unsigned char> file, int &lastaddress, u16c *output){
-    unsigned char buf[2];
-    readChunk(file, lastaddress, 2, buf);
-    *output = (buf[1] << 8) + buf[0];
-}
-VTNCFile VTNCRW::read(std::vector<unsigned char> file)
-{
-    VTNCFile output;
-    u8c TAG [5] = {0};
-    output.layersQuantity;
-    u8c layerKeys[U8Max];
-    output.layersResolution[U8Max];
-    output.colorsQuantity;
-    output.Colors [U8Max];
-    output.framesQuantity;
-
-    int blockOffset = 0;
-
-    readChunk(file, blockOffset, 5, TAG, true); if (memcmp(TAG, _TAGNeeded, 5) == 0) output.isFile = true;
-    readChunk(file, blockOffset, 1, &output.layersQuantity);
-    for (size_t i = 0; i < output.layersQuantity; i++){readChunk(file, blockOffset, 1, &layerKeys[i]);}
-    for (size_t i = 0; i < output.layersQuantity; i++){
-        Resolution res;
-        readChunk16u(file, blockOffset, &res.x);
-        readChunk16u(file, blockOffset, &res.y);
-        output.layersResolution[i] = res;
-    }
-    readChunk(file, blockOffset, 1, &output.colorsQuantity);
-    for (size_t i = 0; i < output.colorsQuantity; i++){
-        u8c address = 0;
-        RGBA temp;
-        readChunk(file, blockOffset, 1, &address);
-        readChunk(file, blockOffset, 1, &temp.R);
-        readChunk(file, blockOffset, 1, &temp.G);
-        readChunk(file, blockOffset, 1, &temp.B);
-        readChunk(file, blockOffset, 1, &temp.A);
-        output.Colors[address] = temp;
-    }
-    readChunk(file, blockOffset, 1, &output.framesQuantity);
-    
-    Layer layers[output.layersQuantity];
-
-   
-    
-    for (size_t layeri = 0; layeri < output.layersQuantity; layeri++)
-    {
-        Layer currentLayer;
-        for (size_t framei = 0; framei < output.framesQuantity; framei++)
-        {
-            Frame currentFrame;
-            for (size_t i = 0; i < output.layersResolution[layeri].x * output.layersResolution[layeri].y; i++)
-            {
-                
-                readChunk(file, blockOffset, 1, &currentFrame.Pixels[i]);
-            }
-            
-            readChunk16u(file,blockOffset,&currentFrame.msDuration);
-            currentLayer.framesArray[framei] = currentFrame;
-            currentLayer.layerKey = layerKeys[layeri];
-            layers[layeri] = currentLayer;
-        }
-    }
-    output.Layers = layers;
-    return output;
-}
-
-std::vector<unsigned char> VTNCRW::write(VTNCFile file) 
-{
-    std::vector<unsigned char> buffer;
-    VTNCFile buffergenerated;
-
-    for (u8c i = 0; i < (sizeof(_TAGNeeded) - 1); i++)
-    {
-        buffer.push_back(_TAGNeeded[i]);
-    }
-    buffer.push_back(file.layersQuantity);
-    for (u8c i = 0; i < file.layersQuantity; i++)
-    {
-        buffer.push_back(file.Layers[i].layerKey);
-    }
-    for (u8c i = 0; i < file.layersQuantity; i++)
-    {
-        buffer.push_back(file.layersResolution[i].x);
-        buffer.push_back((file.layersResolution[i].x<<8));
-        buffer.push_back(file.layersResolution[i].y);
-        buffer.push_back((file.layersResolution[i].y<<8));
-    }
-    buffer.push_back(file.colorsQuantity);
-    for (u8c i = 0; i < file.colorsQuantity; i++)
-    {
-        buffer.push_back(i);
-        buffer.push_back(file.Colors[i].R);
-        buffer.push_back(file.Colors[i].G);
-        buffer.push_back(file.Colors[i].B);
-        buffer.push_back(file.Colors[i].A);
-    }
-    buffer.push_back(file.framesQuantity);
-    for (u8c i_layer = 0; i_layer < file.layersQuantity; i_layer++)
-    {
-        for (u8c i_frame = 0; i_frame < file.framesQuantity; i_frame++)
-        {   
-            Frame currentframe = file.Layers[i_layer].framesArray[i_frame];
-            for (u8c i = 0; i < file.layersResolution[i_layer].x * file.layersResolution[i_layer].y; i++)
-            {
-                buffer.push_back(currentframe.Pixels[i]);   
-            }
-            buffer.push_back(currentframe.msDuration);
-            buffer.push_back(currentframe.msDuration << 8);
-        }
-    }
-    
-    return buffer;
-}
--- src/vtncrw.h
+++ src/vtncrw.h
@@ -1,69 +1,72 @@
 #pragma once
 #include "vector"
 
-//WARNING: THIS REWRITE DOES NOT WORK CURRENTLY SO IT WILL CAUSE A LOT OF COMPILE ERRORS.
+// WARNING: THIS REWRITE DOES NOT WORK CURRENTLY SO IT WILL CAUSE A LOT OF COMPILE ERRORS.
 
-
-//NEED SOME CREATIVE NAME HERE, THE PRESENT ONE IS NOT GOOD AT ALL
-//Basically, it is intendended to be a custom datatype that it's bit size is changeable.
-class resizeable
+namespace VTNCRW
 {
-public:
-    bool* data;
-    unsigned int size;
-    unsigned char* toChar();
-    resizeable(/* args */);
-    ~resizeable();
-};
+    // Rescalable Data
+    // Basically, it is intendended to be a custom datatype that it's bit size is changeable.
+    class rescData
+    {
+    public:
+        // The last bit index of the last byte
+        int lastBitIndex;
+        unsigned char *data;
+        unsigned int bitSize;
+        unsigned char *toChar();
+        rescData(unsigned int bitsize = 8, unsigned int initialsize = 1);
+        ~rescData();
+        rescData operator+(rescData b);
+    };
 
-struct Frame {
-    //The index of the colors inside the ColorBuffer. It is used as a way to not repeat the same 4 bytes for every pixel color.
-    resizeable Pixels;
-    resizeable msDuration;
-};
+    struct Frame
+    {
+        // The index of the colors inside the ColorBuffer. It is used as a way to not repeat the same 4 bytes for every pixel color.
+        rescData Pixels;
+        rescData msDuration;
+    };
 
+    struct Resolution
+    {
+        rescData x;
+        rescData y;
+    };
 
-struct Resolution{
-    resizeable x;
-    resizeable y;
-};
+    struct RGBA
+    {
+        unsigned char R;
+        unsigned char G;
+        unsigned char B;
+        unsigned char A;
+    };
 
+    //"Group" is a generic name for how it is intended to work, as each group can work differently depending of the context.
+    // It can be used as a "layer" category or even as a way to separate multiple images in only one file.
+    struct Group
+    {
+        Resolution resolution;
 
-struct RGBA{
-    unsigned char R;
-    unsigned char G;
-    unsigned char B;
-    unsigned char A;
-};
+        // An array of frames.
+        Frame *frames;
 
+        // A set of numbers that is interpreted by some software in the way the developers intended.
+        // Works as a key.
+        unsigned char modifiers;
+    };
+    struct Internals
+    {
+        unsigned char TAGNeeded[6] = "VTNCI";
+    };
 
-//"Group" is a generic name for how it is intended work, as each group can work differently depending of the context.
-//It can be used as a "layer" category or even as a way to separate multiple images in only one file.
-struct Group{
-    Resolution resolution;
-
-    //An array of frames.
-    Frame* frames;
-
-    //A set of numbers that is interpreted by some software in the way the developers intended.
-    //Works as a key.
-    unsigned char modifiers;
-};
-
-class VTNCFile
-{
-public:
-    RGBA *ColorBuffer;
-    Group *GroupList;
-};
-
-class VTNCRW
-{
-private:
-    unsigned char TAGNeeded [6] = "VTNCI";
-public:
-    VTNCRW();
-    VTNCFile create(unsigned char layerQuantity, Resolution EachLayerResolution[], unsigned char ColorsQuantity, unsigned char EndsAnimationAt);
+    class VTNCFile
+    {
+    public:
+        RGBA *ColorBuffer;
+        Group *GroupList;
+        VTNCFile();
+    };
+    VTNCFile create(Group *GroupList);
     VTNCFile read(std::vector<unsigned char> file);
     std::vector<unsigned char> write(VTNCFile file);
-};
+} // namespace VTNCRW
